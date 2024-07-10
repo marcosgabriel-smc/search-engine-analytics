2d0ea5b03737a9f808a63ba1206f38bb5b42da61
Marcos Gabriel Costa <marcosgabriel.smc@gmail.com>
2024-07-10 14:57:22 -0300

    Adding Article validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,9 @@
 class Article < ApplicationRecord
+  validates :title, presence: true, uniqueness: true
+  validates :content, presence: true, length: {
+    minimum: 300,
+    too_short: 'Your content must have at least 300 characters.',
+    maximum: 3000,
+    too_long: 'Your content must not exceed 3000 characters.'
+  }
 end
