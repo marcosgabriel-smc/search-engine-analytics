1154abd440a37b764579081d112baf246fdc98b6
Marcos Gabriel Costa <marcosgabriel.smc@gmail.com>
2024-07-10 16:01:12 -0300

    Implementing simple search bar with pg_search

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ApplicationRecord
+  include PgSearch::Model
+  pg_search_scope :search_by_title, against: :title
+
   validates :title, presence: true, uniqueness: true
   validates :content, presence: true, length: {
     minimum: 300,
