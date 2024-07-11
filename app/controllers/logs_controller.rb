8dbe42d960779c1f1d0f5951ffd690b8967776d7
Marcos Gabriel Costa <marcosgabriel.smc@gmail.com>
2024-07-11 11:24:48 -0300

    Implementing validations for the Log

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,6 +1,12 @@
 class LogsController < ApplicationController
   def create
     @log = Log.new(log_params)
+
+    unless valid_input?(@log.input) && valid_ip?(@log.ip)
+      render json: { error: 'Invalid input or IP address' }, status: :unprocessable_entity
+      return
+    end
+
     api_key = ENV['GEO_LOCATION_API']
     response = HTTParty.get("https://api.ipgeolocation.io/ipgeo?apiKey=#{api_key}&ip=#{@log.ip}")
     location_data = response.parsed_response
@@ -8,14 +14,26 @@ class LogsController < ApplicationController
     country = location_data['country_name']
     @log.city = city
     @log.country = country
+    Rails.logger.info "LOG 1: #{@log.inspect}"
 
-    @log.save
-
-    # render json: @log
+    if @log.save
+      render json: @log, status: :created
+    else
+      render json: @log.errors, status: :unprocessable_entity
+    end
   end
 
   private
   def log_params
     params.require(:log).permit(:input, :ip)
   end
+
+  def valid_input?(input)
+    input.present? && input.length >= 3
+  end
+
+  def valid_ip?(ip)
+    !!(/\A(?:[0-9]{1,3}\.){3}[0-9]{1,3}\z/.match(ip))
+  end
+
 end
--- app/models/log.rb
+++ app/models/log.rb
@@ -1,2 +1,6 @@
 class Log < ApplicationRecord
+  validates :input, presence: true, length: {minimum: 3}
+  validates :ip, presence: true, format: { with: /\A(?:[0-9]{1,3}\.){3}[0-9]{1,3}\z/, message: "must be a valid IPv4 address" }
+  validates :city, presence: true
+  validates :country, presence: true
 end
